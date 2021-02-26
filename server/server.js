c2888c82a9aba3d5948a5e40b8ff887605965ad4
manulangat <emmanuelthedeveloper@gmail.com>
2021-02-26 23:46:07 +0300

    ft-setting-up-backend-docker

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ dotenv.config()
 const app = express();
 
 
-app.use('/', (req,res) => res.send('hello world'))
+app.use('/', (req,res) => res.send('hello worlds'))
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, console.log(`Books app running in PORT ${PORT} in ${process.env.NODE_ENV} mode`.yellow.underline.bold))
